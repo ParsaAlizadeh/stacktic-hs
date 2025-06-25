f5bd9cb7f327926e0b1d25973f2a0155122f21eb
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2025-06-25 11:35:13 +0330

    Use shrink technology from QuickCheck

--- test/Main.hs
+++ test/Main.hs
@@ -9,27 +9,24 @@ import Test.Hspec
 import Test.QuickCheck
 import Test.Hspec.QuickCheck
 
-import Data.Functor.Identity
 import qualified Language.Stacktic as S
+import Data.Functor.Identity
 
 fib :: Integer -> Integer
 fib n = go n 0 1 where
   go 0 !a _ = a
   go n !a !b = go (n - 1) b (a + b)
 
-chooseNonNegative :: Integer -> Gen Integer
-chooseNonNegative n = chooseInteger (0, n)
-
 main :: IO ()
 main = hspec do
   describe "library" do
     describe "fib" do
-      prop "calculates n'th fib number" $
-        forAll (chooseNonNegative 1000) \n ->
+      prop "calculates n'th fib number" $ 
+        withMaxSize 1000 \ (NonNegative n) ->
           runIdentity (S.fib ((), n)) == ((), fib n)
     describe "fibfix" do
       prop "calculates n'th fib number, using y combinator" $ 
-        forAll (chooseNonNegative 20) \n ->
+        withMaxSize 20 \ (NonNegative n) ->
           runIdentity (S.fibfix ((), n)) == ((), fib n)
     describe "sum" do
       prop "same as Prelude.sum" \ (xs :: [Integer]) ->
